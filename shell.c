eba5bf525a42cef9538d54a93b6d0a94997245b8
Sunan Xiang <xsunan@cs.uchicago.edu>
2020-04-26 15:09:59 -0500

    solve segmfault

--- shell.c
+++ shell.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-#define COMMAND_LEN 32
+#include <stdlib.h>
+#define COMMAND_LEN 50
 #define ARGV_LEN 150
-#define COMMAND_NUM 10
+#define COMMAND_NUM 20
 
 int extract_all_commands(char* input, char** all_commands){
 	int concurrent;
-	const char delim[2]= " &;";
+	const char delim[3]= "&;\n";
 
 	if(strchr(input, '&') != NULL){
 		concurrent = 1;
@@ -14,28 +15,29 @@ int extract_all_commands(char* input, char** all_commands){
 		concurrent = 0;	
 	} 
 
-
 	// divide multiple commands
 	char *command = strtok(input, delim);   	
 	int i=0;
 	while(command != NULL ) {
+		all_commands[i] = (char *) malloc(sizeof(char)*COMMAND_LEN);
 		all_commands[i++] = command;
 	  	command = strtok(NULL, delim);
 	}
+	all_commands[i] = NULL;
 	return concurrent;
 }
 
-void sub_commands(char* command){
+void sub_commands(char* command, char** all_sub_cmds){
 
 	/* find pipelined commands*/
-	const char delim_pipe[] = " >|";
-	char *all_sub_cmds[COMMAND_NUM];
+	const char delim_pipe[] = ">|\n";
+
 	// parse arguements
 	char *sub_cmd = strtok(command, delim_pipe);   	
 	int i=0;
-	while(sub_cmd != NULL ) {
-		printf( "%s\n", sub_cmd );
-		all_sub_cmds[i++] = sub_cmd;
+	while(sub_cmd != NULL && strlen(sub_cmd)>0) {
+		all_sub_cmds[i] = (char *) malloc(sizeof(char)*COMMAND_LEN);
+		all_sub_cmds[i++] =sub_cmd;
 	  	sub_cmd = strtok(NULL, delim_pipe);
 	}
 
@@ -43,7 +45,6 @@ void sub_commands(char* command){
 
 int main(){
 	char input_line[COMMAND_LEN];
-	const char delim[] = " \t"; 
   
     char *all_commands[COMMAND_NUM];
     char *command;
@@ -56,12 +57,16 @@ int main(){
 	int concurrent = extract_all_commands(input_line, all_commands);
 	
 	int i=0;
+	char *all_sub_cmds[COMMAND_NUM];
 
 	while(all_commands[i]!=NULL){
-		sub_commands(all_commands[i++]);
+		sub_commands(all_commands[i++],all_sub_cmds);
 	}
 
 	
 
 
 }
+
+
+
